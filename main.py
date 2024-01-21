45f6bcf81a5a95259eca5f2a2fd9aeb7edbb98f7
David Requeno <davidrequeno52@gmail.com>
2024-01-21 12:46:18 -0600

    one_task

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
-from database import get_all_task
+from fastapi import FastAPI, HTTPException
+from database import get_all_task, create_task, get_one_task
+from models import Task
 
 app = FastAPI()
 
@@ -12,9 +13,17 @@ async def get_tasks():
     tasks = await get_all_task()
     return tasks
 
-@app.post('/api/tasks')
-async def create_tasks():
-    return 'create tasks'
+@app.post('/api/tasks', response_model=Task)
+async def create_tasks(task: Task):
+
+    task_found = await get_one_task(task.title)
+    if task_found:
+        raise HTTPException(409, 'La tarea ya existe')
+
+    response = await create_task(task.dict())
+    if response:
+        return response
+    raise HTTPException(400, "Algo salio mal")
 
 @app.get('/api/tasks/{id}')
 async def get_tasks():
--- models.py
+++ models.py
@@ -1,7 +1,25 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional
+from bson import ObjectId
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v, values):
+        if not ObjectId.is_valid(v):
+            raise ValueError('Invalid ObjectId')
+        return str(v)
 
 class Task(BaseModel):
-    id: str
+    id: PyObjectId = Field(default_factory=PyObjectId, alias='_id')
     title: str
-    decription: str
+    description: Optional[str] = None
     completed: bool = False
+
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
+        json_encoders = {ObjectId: str}
